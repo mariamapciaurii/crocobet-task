3de8bbce55201d437489cc5e5b0e6f937d73f77d
Mariam Aptsiauri <72245573+mariamapciaurii@users.noreply.github.com>
2021-06-15 22:32:06 +0400

    js

--- js/main.js
+++ js/main.js
@@ -9,49 +9,45 @@ fetch('https://jsonplaceholder.typicode.com/users')
     console.log('error: ' + err);
 });
 
-function appendData(data) {
-var mainContainer = document.getElementById("users");
-for (var i = 0; i < data.length; i++) {
-    var li = document.createElement("li");
-    li.innerHTML =  data[i].username;
-    li.classList.add('item');
-    li.dataset.userId = data[i].id;
-    li.addEventListener('click', (event) => getPosts(event))
-    mainContainer.appendChild(li);
-}
 
+function appendData(data) {
+    let mainContainer = document.getElementById("users");
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML =  data[i].username;
+        div.classList.add('item');
+        div.dataset.userId = data[i].id;
+        div.addEventListener('click', (event) => getPosts(event))
+        mainContainer.appendChild(div);
+    }
 }
 
 function cleanPosts() {
-var users = document.querySelectorAll('.item ul');
-for(var i = 0; i < users.length; i++) {
-    if(users[i]) {
+    var users = document.querySelectorAll('.item ul');
+    for(var i = 0; i < users.length; i++) {
+        if(users[i]) {
         users[i].style.display = 'none';
     }
 }
 }
 
 function getPosts(event) {
-
-var userId = event.target.dataset.userId;
-
-fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+    var userId = event.target.dataset.userId;
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
     .then(response => response.json())
     .then(json => renderPosts(json, event.target))
 }
 
 function renderPosts(posts, target) {
-var postsList = target.childNodes[1];
-
-cleanPosts();
-
-if(postsList){
+    var postsList = target.childNodes[1];
+    cleanPosts();
+    if(postsList){
     postsList.style.display = 'block';
+
 } else {
     var list = document.createElement("ul");
 
-    for (var i = 0; i < posts.length; i++) {
-
+     for (var i = 0; i < posts.length; i++) {
         var item = document.createElement("li");
         var liTitle = document.createElement("strong");
         var liBody = document.createElement("p");
